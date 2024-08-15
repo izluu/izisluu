50acde2ebb36bcbf9e583eb0cbf46d8d614c46c2
izluu <163074579+izluu@users.noreply.github.com>
2024-08-15 23:22:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
+const btnGroup = document.querySelector(".btn-group");
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "I love you too! 😘";
   gif.src = "giphy3.webp";
-  
   
   yesBtn.style.display = "none";
   noBtn.style.display = "none";
 });
 
 noBtn.addEventListener("mouseover", () => {
+  const groupRect = btnGroup.getBoundingClientRect();
   const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
+  const maxX = groupRect.width - noBtnRect.width;
+  const maxY = groupRect.height - noBtnRect.height;
 
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
 
+  noBtn.style.position = 'absolute'; // Chuyển vị trí nút No sang vị trí tuyệt đối khi di chuyển
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
 });
